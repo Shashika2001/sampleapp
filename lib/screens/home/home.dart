dbfbcd3f24c2584448ac5091296584e5982f9ef9
Shashika Ekanayaka <138883821+Shashika2001@users.noreply.github.com>
2024-02-08 22:17:08 +0530

    Update home.dart

--- lib/screens/home/home.dart
+++ lib/screens/home/home.dart
@@ -1,4 +1,7 @@
 import "package:flutter/material.dart";
+import "package:sampleapp/constants/colors.dart";
+import "package:sampleapp/constants/description.dart";
+import "package:sampleapp/services/auth.dart";
 class Home extends StatefulWidget {
   const Home({super.key});
 
@@ -7,10 +10,65 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  //create a object from authservices
+  final AuthServices _auth=AuthServices();
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text("Home"),
+    return MaterialApp(
+      home: Scaffold(
+        backgroundColor: bgBlack,
+
+        appBar: AppBar(
+          elevation: 0,
+          backgroundColor: bgBlack,
+          
+
+          actions: [
+
+            ElevatedButton(
+              style: ButtonStyle(backgroundColor: MaterialStatePropertyAll(bgBlack),
+              ),
+              onPressed: () async{
+              await _auth.signOut();
+            }, child: const Icon(Icons.logout),)
+          ],
+        ),
+        body: Padding(
+          padding: const EdgeInsets.all(20.0),
+          child: Center(
+            child: Column(
+              children: [
+                Text("Home",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 20,
+                  fontWeight: FontWeight.w800
+                ),
+                ),
+                SizedBox(height: 30,),
+                const Text(description,
+                style: TextStyle(
+                  color: textLight,
+                  fontSize: 10,
+                  fontWeight: FontWeight.w300),
+                
+                ),
+
+                Center(child: Image.asset('assets/boy24.jpg',
+              height: 200,
+              )
+              ),
+
+
+              ],
+            
+            
+            ),
+          ),
+        ),
+      ),
     );
   }
 }

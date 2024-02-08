794f7ec643c813c96d528bbf405e4deb9312e2c0
Shashika Ekanayaka <138883821+Shashika2001@users.noreply.github.com>
2024-02-08 22:19:28 +0530

    Update auth.dart

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -5,7 +5,7 @@ import 'package:sampleapp/services/models/UserModel.dart';
 
 class AuthServices{
 
-//forebase instance
+//firebase instance
 
 final FirebaseAuth _auth=FirebaseAuth.instance;
 
@@ -21,7 +21,7 @@ Stream<UserModel?> get user {
   //sign in anonymous
 Future signInAnonymously() async{
  try{
-   UserCredential result= await signInAnonymously();
+   UserCredential result= await _auth.signInAnonymously();
   User? user=result.user;
   return _userWithFirebaseUserUid(user);
 
@@ -32,7 +32,26 @@ Future signInAnonymously() async{
 }
 
   //register using email & password
+  Future registerWithEmailAndPassword(String email,String passwprd)async{
+    try{
+      UserCredential result= await _auth.createUserWithEmailAndPassword(email: email, password:passwprd);
+      User? user=result.user;
+
+    }catch(err){
+      print(err.toString()
+      );
+      return null;
+    }
+  }
   //sign in using email & password
   //sign in using email
   //sign out
+  Future signOut() async{
+    try{
+      return await  _auth.signOut();
+    }catch (err){
+      print(err.toString());
+      return null;
+    }
+  }
 }

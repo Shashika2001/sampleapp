3a588b933cd9e472645c8723c6fa66123f045145
Shashika Ekanayaka <138883821+Shashika2001@users.noreply.github.com>
2024-02-08 22:18:14 +0530

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sampleapp/firebase_options.dart';
 import 'package:sampleapp/screens/wrapper.dart';
+import 'package:sampleapp/services/auth.dart';
+import 'package:sampleapp/services/models/UserModel.dart';
 
 void main()async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -24,6 +27,13 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Wrapper(),);
+    return StreamProvider<UserModel?>.value(initialData: UserModel(uid: ""),
+    value:AuthServices().user,
+    child: MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Wrapper(),
+    ),
+    
+    );
   }
 }

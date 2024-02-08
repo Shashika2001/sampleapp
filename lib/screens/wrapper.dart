d64893e4b2d71ac1ed11e89a7b6d99c54a2d1df3
Shashika Ekanayaka <138883821+Shashika2001@users.noreply.github.com>
2024-02-08 22:17:37 +0530

    Update wrapper.dart

--- lib/screens/wrapper.dart
+++ lib/screens/wrapper.dart
@@ -1,14 +1,22 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:sampleapp/screens/authentication/authenticate.dart';
 import 'package:sampleapp/screens/home/home.dart';
+import 'package:sampleapp/services/models/UserModel.dart';
 
 class Wrapper extends StatelessWidget {
   const Wrapper({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Authenticate(),
-    );
+//the user data that the provider provides this can be a user data or can be null
+final user = Provider.of<UserModel?>(context);
+
+if(user==null){
+  return Authenticate();
+}else{
+  return Home();
+}
+
   }
 }
